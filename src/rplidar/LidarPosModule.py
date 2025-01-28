0bf9d74c3906ffc6fe56bcca345b1d0a94274314
ppenar <ppenar@prz.edu.pl>
2025-01-28 08:50:35 +0100

    zmiana w LidarPosModule

--- src/rplidar/LidarPosModule.py
+++ src/rplidar/LidarPosModule.py
@@ -3,13 +3,14 @@ from time import sleep
 import utils
 from configModel import ModuleConfig
 from threading import Thread, Event, Lock
-from rplidar.pyrplidar import PyRPlidar, PyRPlidarConnectionError
 import numpy as np
 
 
 from breezyslam.algorithms import RMHC_SLAM
 from breezyslam.sensors import RPLidarA1 as LaserModel
 
+from rplidar import RPLidar, RPLidarException
+from datetime import datetime
 
 
 
@@ -47,6 +48,13 @@ class LidarModule(Thread):
         self.currentPosInterval = 0
 
         self.slam = RMHC_SLAM(LaserModel(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+        self.previous_distances = None
+        self.previous_angles    = None
+        
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+
 
         glob.log.info("Thread %s create",self.name)
 
@@ -57,15 +65,20 @@ class LidarModule(Thread):
             self.ioManager.tui.updateStatus(self.name,"Port is close")
             return False
         
-        self.lidar = PyRPlidar()
+        self.lidar = RPLidar(self.port, self.baundrate, timeout=3)
         try:
-            self.lidar.connect(self.port,self.baundrate,timeout=3)
+            self.lidar.connect()
             self.lidar.get_info()
-        except PyRPlidarConnectionError as err:
+        except RPLidarException as err:
             self.ioManager.tui.updateStatus(self.name,"Lidar not found")
             return False
 
         self.ioManager.tui.updateStatus(self.name,"INIT: Lidar connected")
+
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        fileName = f"{timestamp}.txt"
+        self.outfile = open(fileName, 'w')
         return True
     
 
@@ -76,21 +89,40 @@ class LidarModule(Thread):
         self.lock.release()
 
 
+        iterator = self.lidar.iter_scans()
+
+
+
 
         
         while True:
-            scan_generator = self.lidar.start_scan()
-            for scan in scan_generator():
+            distances = []
+            angles = []
+            for item in next(iterator):
+                distances.append(item[2])
+                angles.append(item[1])
 
-
-                
-                angleIndex = np.rint(scan.angle).astype(np.int16)
+                angleIndex = np.rint(item[1]).astype(np.int16)
                 if angleIndex==360:
                      angleIndex=0
+                self.lidarNpArr[359-angleIndex]=item[2]
+            
+                if len(distances) > 180:
+                    self.slam.update(distances, scan_angles_degrees=angles)
+                    self.previous_distances = distances.copy()
+                    self.previous_angles    = angles.copy()
 
-                
-                self.lidarNpArr[359-angleIndex]=scan.distance
-                    
+            # If not adequate, use previous
+                elif self.previous_distances is not None:
+                    self.slam.update(self.previous_distances, scan_angles_degrees=self.previous_angles)
+
+            # Get current robot position
+                self.x, self.y, self.theta = self.slam.getpos()
+                self.outfile.write(str(self.x) + ' ' + str(self.y) + ' ' + str(self.theta) + '\n')
+
+
+
+
                 if self.closeEvent.is_set():
                     self.afterClose()
                     break
@@ -106,15 +138,14 @@ class LidarModule(Thread):
 
     def afterSendByOutput(self):
 
-        if self.currentPosInterval == self.posInterval and np.all(self.lidarNpArr!=0):
-            self.slam.update(self.lidarNpArr,np.array(range(0,360,1)))
-            x,y,theta = self.slam.getpos()
+        if self.currentPosInterval == self.posInterval:
+            
             self.sendData[0]=25
-            self.sendData[1]=x
-            self.sendData[2]=y
-            self.sendData[3]=theta
+            self.sendData[1]=self.x
+            self.sendData[2]=self.y
+            self.sendData[3]=self.theta
 
-            for i in range(4,self.numInt16-1):\
+            for i in range(4,self.numInt16-1):
                 self.sendData[i]=100
             self.sendData[self.numInt16-1]=(self.posX+self.posY)/2
 
